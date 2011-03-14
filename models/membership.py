b4f2cc9b260f2cd386ac97340bf4f7160af0a450
Leaf Johnson <leaf@linux-trgd.site>
2011-03-14 22:26:17 +0800

    ADD membership model

---
+++ models/membership.py
@@ -0,0 +1,30 @@
+'''Copyright(C): Leaf Johnson 2011
+
+    This file is part of makeclub.
+
+    makeclub is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    makeclub is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with makeclub.  If not, see <http://www.gnu.org/licenses/>.
+
+ 
+ 
+'''
+from google.appengine.ext import db
+from properties import MoneyProperty
+from club import Club
+
+class Membership(db.Model):
+	user = db.UserProperty()
+	club = db.ReferenceProperty(Club)
+	name = db.StringProperty(multiline=False) #Name display in this club
+	balance = MoneyProperty()
+	mail = db.EMailProperty()
