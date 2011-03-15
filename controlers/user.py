85ab8524dfd7e7c37734d277a7d4194947df818c
Leaf Johnson <leaf@linux-trgd.site>
2011-03-15 22:07:27 +0800

    USE template engine in user.py

--- controlers/user.py
+++ controlers/user.py
@@ -19,25 +19,23 @@
  
 '''
 from google.appengine.ext import webapp
-from google.appengine.ext.webapp import template
-from google.appengine.ext.webapp.util import run_wsgi_app
-import os
+from template import render
 
 class UserList(webapp.RequestHandler):
 	def __init__(self,
-			template=os.path.join(os.path.dirname(__file__), '../templates/default/userlist.html'), *args, **kw ):
+			template='userlist.html', *args, **kw ):
 		webapp.RequestHandler.__init__(self, *args, **kw)
 		self.template = template
 
 	def get(self):
-		self.response.out.write (template.render(self.template, locals()) )
+		self.response.out.write (render(self.template, locals()) )
 
 class UserView(webapp.RequestHandler):
 	def __init__(self, 
-			template=os.path.join(os.path.dirname(__file__), '../templates/default/userview.html'), *args, **kw ):
+			template='userview.html', *args, **kw ):
 		webapp.RequestHandler.__init__(self, *args, **kw)
 		self.template = template
 
 	def get(self):
-		self.response.out.write (template.render(self.template, locals()) )
+		self.response.out.write (render(self.template, locals()) )
 
