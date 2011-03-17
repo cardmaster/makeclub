eb5affe36d2f89f934eb269e92773c7227091c61
Leaf Johnson <leafjohn@gmail.com>
2011-03-17 22:51:03 +0800

    ADD URL Objective code

--- controlers/url.py
+++ controlers/url.py
@@ -32,16 +32,57 @@ We use this rule:
 							field in request data, will cause this targetUser join this activity
 '''
 import re
+import os.path 
+pathjoin = os.path.join
 
-
-def extPattern(base):
-	return base + '($|/.*)'
 
 cluburl = "/club"
 clubjoin = "/club/member"
 clubedit = "/club/%s/edit"
 clublist = "/clubs"
 memberurl = "/member"
+
+
+def extPattern(base):
+	return base + '($|/.*)'
+class ModuleUrlConf:
+	@staticmethod
+	def generatePattern(base):
+		return (base % '(\S+)')
+	def __init__(self, base, pattern=''):#Base url must have a %s, to specify the variable part
+		self.base = base
+		if (not pattern):
+			self.pattern = ModuleUrlConf.generatePattern(base)
+		else:
+			self.pattern = pattern
+	def path(self, *args):
+		return base % args
+	def analyze(self, path):
+		reg = re.compile(self.pattern)
+		mat = reg.match (path)
+		if (mat):
+			try:
+				return mat.groups()[1:]
+			except:
+				return []
+		else:
+			return []
+
+from helper import splitPath
+class MemberUrlConf(ModuleUrlConf):
+	def path(self, slug, user=''):
+		return ModuleUrlConf.path(self, slug, user)
+	def analyze(self, path):
+		result = splitPath(path, '/member', 2)
+		return result
+
+urldict = dict (
+	clublist = ModuleUrlConf('/clubs', extPattern('/clubs') ),
+	clubview = ModuleUrlConf('/club/%s', '/club/(\S+)/?$'),
+	clubedit = ModuleUrlConf('/club/%s/edit', '/club/(\S+)/edit/?$'),
+	member   = MemberUrlConf('/member/%s/%s', '/member/(\S+)/(\S+)')
+)
+
 class UrlConf:
 	def memberPath(self, slug='', user=''):
 		return (memberurl + '/' + slug + '/' + user)
