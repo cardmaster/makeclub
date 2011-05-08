6d885bd5d6d0d7a2ff33893e2a9cc8d8cf07cf1f
Leaf Johnson <leafjohn@gmail.com>
2011-05-08 20:50:14 +0800

    This script could chage function calls in give text

---
+++ scripts/changeparamorder.pl
@@ -0,0 +1,94 @@
+#!/usr/bin/perl -w
+use strict;
+
+my $dbgOn = 0;
+sub splitParams;
+sub dbg
+{
+	print "#DBG", join (' ', @_), "\n" if $dbgOn;
+}
+
+my $func = 'errorPage'; #Function Name
+my $oldpos = 2; #Param position before swap
+my $newpos = 0; #Param position after swap
+
+my $funcbegin = 0;
+my $funccall = '';
+#Will be a filter first
+while (my $ln = <STDIN>) {
+	if ($ln =~ m/$func\s*\(/) { #Mark function begin
+		$funcbegin = 1;
+	}
+
+	if ($funcbegin) {
+		$funccall = $funccall.$ln;
+		chomp $funccall;
+		#Get matched braces
+		my $temp = $funccall;
+		my $matched;
+		while ($temp =~ s/\(([^()]*)\)//) {
+			$matched = $1;
+			dbg "matched:", $matched;
+		}
+		if (index($temp, '(')>=0) {
+			dbg "Incomplete CALL '$funccall' is not complete"; 
+		} else {
+			dbg "Complete Call '$funccall'";
+			$funccall =~ m/\((.*)\)/;
+			$matched = $1;
+			my @params = splitParams($matched);
+			dbg "Param List: ", join (', ', @params);
+
+			my $swap = $params[$newpos];
+			$params[$newpos] = $params[$oldpos];
+			$params[$oldpos] = $swap;
+
+			my $newpstr = join (', ', @params);
+			dbg "Adjusted param list: ", $newpstr;
+
+			$funccall =~ s/($func\s*)\(.*\)/$1($newpstr)/;
+			print $funccall, "\n";
+
+			$funccall = '';
+			$funcbegin = 0;
+		}
+	} else {
+		print $ln;
+	}
+}
+
+sub splitParams
+{
+	my ($pstr) = @_;
+	dbg "Param String: ", $pstr;
+	my @params;
+	my @chars = split '', $pstr;
+	my $inspecial = '';
+	my $prevpos = 0;
+	my $pos = 0;
+	foreach my $char (@chars) {
+		if ($inspecial) {
+			if ($char eq $inspecial) {
+				dbg "Out of Special char='$char'", " pos=$pos";
+				$inspecial = '';
+			}
+		} else {
+			if ($char eq '('){
+				dbg "In Special, char ='$char'", " pos=$pos";
+				$inspecial = ')';
+			}
+			if (index('\'"', $char) >= 0) {
+				dbg "In Special, char ='$char'", " pos=$pos";
+				$inspecial = $char;
+			}
+		}
+		if ((not $inspecial) and ($char eq ',')) {
+			push @params, substr($pstr, $prevpos, $pos - $prevpos);
+			dbg "SUBSTR: ", substr($pstr, 0, $pos + 1);
+			$prevpos = $pos + 1;
+		}
+		++$pos;
+	}
+	push @params, substr($pstr, $prevpos);
+	return @params;
+}
