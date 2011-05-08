95814a915d925f97d1ef79876d0822b7a6c77776
Leaf Johnson <leafjohn@gmail.com>
2011-05-08 13:54:05 +0800

    Add new quick page generator

---
+++ controlers/infopage.py
@@ -0,0 +1,32 @@
+'''Copyright(C): Leaf Johnson 2011
+
+	This file is part of makeclub.
+
+	makeclub is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	makeclub is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with makeclub.  If not, see <http://www.gnu.org/licenses/>.
+
+ 
+ 
+'''
+
+'''
+Infopage is use to generate some tiny information
+'''
+
+from template import render
+
+InfoPageTempl = 'infopage.html'
+
+def infoPage(response, title = '', message = '', redirect = ''):
+	vars = dict(title = title, pageTitle = title, message = message, redirect = redirect)
+	response.out.write (render(infoPageTempl, vars))
